03425c5cf0af752ab4f8a0ae5760ef34f94425d0
Amélien Bonneau <amelienbonneau89@hotmail.fr>
2024-09-22 17:53:25 +0000

    test6

--- cast-service/app/main.py
+++ cast-service/app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from app.api.casts import casts
 from app.api.db import metadata, database, engine
 
+# Créer toutes les tables définies dans le métadonnées
 metadata.create_all(engine)
 
+# Initialiser l'application FastAPI
 app = FastAPI(openapi_url="/api/v1/casts/openapi.json", docs_url="/api/v1/casts/docs")
 
 @app.on_event("startup")
@@ -14,4 +16,5 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+# Inclure le routeur pour les casts
 app.include_router(casts, prefix='/api/v1/casts', tags=['casts'])
